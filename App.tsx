017ceb09fb2fcde906d53586e77d74335e81d2ad
jose87correia <jose87correia@gmail.com>
2020-05-07 16:18:21 +0100

    Defined folder struct

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Hello from './components/Hello';
+import './styles/style.css';
+
+interface AppProps { }
+interface AppState {
+  name: string;
+}
+
+class App extends Component<AppProps, AppState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'React'
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <Hello name={this.state.name} />
+        <p>
+          Start editing to see some magic happen :)
+        </p>
+      </div>
+    );
+  }
+}
+export default App;
